6e219bf694ee36e08c19123b67250478f1d38cdc
Tejas Kumar <tejas@contiamo.com>
2018-06-01 08:15:50 +0200

    Add meta info to slides

--- src/Slide.js
+++ src/Slide.js
@@ -4,6 +4,7 @@ import "./Slide.css";
 
 const Slide = ({ title, subtitle }) => (
   <div className="slide">
+    <div className="meta">@tejaskumar_</div>
     <h1>{title}</h1>
     {subtitle && <h2>{subtitle}</h2>}
   </div>
